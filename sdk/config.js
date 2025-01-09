364a6c1355a9ffdccf442e05b1c2952b87fed403
OfficialBuildAI <himonday379@gmail.com>
2025-01-09 18:21:18 +0700

    Create config.js

---
+++ sdk/config.js
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  network: "devnet", // Options: "mainnet-beta", "testnet", "devnet"
+  programId: "BuildAIxxxxxxx...", // Replace with your deployed program ID
+  rpcEndpoint: "https://api.devnet.solana.com", // Solana RPC endpoint
+};
